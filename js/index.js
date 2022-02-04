6683f23478fd4a6a414d4be058ac6ad90278fe37
David Perelroizen <david.perelroizen@gmail.com>
2022-02-04 17:52:27 +0100

    Added the short description to the best movie area

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
         var filmList = data;
         console.log(filmList);
         document.getElementById("Best-movie-title").innerHTML = "<h1>"+filmList.results['0'].title+"<\h1>";
+        document.getElementById("Best-rated-movie-image").innerHTML = "<img src="+filmList.results['0'].image_url+"><\img>";
+        fetch(filmList.results['0'].url)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            var filmData = data;
+            console.log(filmData);
+            document.getElementById("Best-movie-summary").innerHTML = "<p>"+filmData.description+"<\p>";
+        })
     })
 
 
