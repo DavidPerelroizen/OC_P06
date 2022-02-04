6dedab0107119e8e16dec6dd34f72c34a3113a5b
David Perelroizen <david.perelroizen@gmail.com>
2022-02-04 16:53:03 +0100

    Implemented the best movie name

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-function getBestMovieImage(){
-    let bestMovieDico = fetch("http://localhost:8000/api/v1/titles?imdb_score=1.0");
-    document.getElementById("Best-rated-movie-image").innerHTML = "<img src=" + bestMovieDico.image_url + "alt= 'Best movie image'"
-};
 
-getBestMovieImage();
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        var filmList = data;
+        console.log(filmList);
+        document.getElementById("Best-movie-title").innerHTML = "<h1>"+filmList.results['0'].title+"<\h1>";
+    })
+
+
+
