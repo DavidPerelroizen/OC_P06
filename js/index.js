c2ca95f855f964b2e97e8e3218088df2d8519d76
David Perelroizen <david.perelroizen@gmail.com>
2022-02-07 11:02:12 +0100

    Added the 4 best rated movies to the carrousel

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
         console.log(filmList);
         document.getElementById("Best-movie-title").innerHTML = "<h1>"+filmList.results['0'].title+"<\h1>";
         document.getElementById("Best-rated-movie-image").innerHTML = "<img src="+filmList.results['0'].image_url+"><\img>";
+        var filmRanking = filmList.results;
         fetch(filmList.results['0'].url)
         .then(response => {
             return response.json()
@@ -16,6 +17,25 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
             var filmData = data;
             console.log(filmData);
             document.getElementById("Best-movie-summary").innerHTML = "<p>"+filmData.description+"<\p>";
+        })
+        .then(data => {
+            fetch(filmList.next)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                var filmListNext = data;
+                for (film of filmListNext.results) {
+                    filmRanking.push(film)
+                };
+                console.log(filmRanking)
+                
+            })
+            .then(data => {
+                for (let i=0; i < 4; i++){
+                    document.getElementById("Best-movie-box-"+(i+1)).innerHTML = "<img src="+filmRanking[i].image_url+"><\img>";
+                }
+            })
         })
     })
 
